bd6af06a70deb8756fde44a42374e6c66c54b127
DylanRowe88 <98070240+DylanRowe88@users.noreply.github.com>
2023-04-05 15:53:26 -0400

    Added some stuff

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,15 @@ namespace DylanRowe_RCR_SnakeGame
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
